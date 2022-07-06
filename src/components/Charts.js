7d2f2fa6e2700e1176ed5b9e818fd1011809029b
olejandro <ob@facilitate.energy>
2022-07-06 11:28:55 -0500

    Don't render charts when chartsInfo[chart] is null (closes #22)

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -23,25 +23,31 @@ function Charts(props) {
       xs={"auto"}
       className="charts py-2 justify-content-center justify-content-md-start"
     >
-      {charts.map((chart, idx) => (
-        <Col className="p-2" key={idx}>
-          <Card style={{ maxWidth: chartWidth }}>
-            <Card.Header>
-              {(chartsTitles && chartsTitles[chart]) || chart}
-            </Card.Header>
-            <Chart
-              chartName={chart}
-              {...props}
-              {...chartsInfo[chart]}
-              chartWidth={chartWidth}
-              widthScaling={widthScaling}
-            />
-            <Card.Footer>
-              <ChartLegend {...chartsInfo[chart]} seriesTitles={seriesTitles} />
-            </Card.Footer>
-          </Card>
-        </Col>
-      ))}
+      {charts.map(
+        (chart, idx) =>
+          chartsInfo[chart] && (
+            <Col className="p-2" key={idx}>
+              <Card style={{ maxWidth: chartWidth }}>
+                <Card.Header>
+                  {(chartsTitles && chartsTitles[chart]) || chart}
+                </Card.Header>
+                <Chart
+                  chartName={chart}
+                  {...props}
+                  {...chartsInfo[chart]}
+                  chartWidth={chartWidth}
+                  widthScaling={widthScaling}
+                />
+                <Card.Footer>
+                  <ChartLegend
+                    {...chartsInfo[chart]}
+                    seriesTitles={seriesTitles}
+                  />
+                </Card.Footer>
+              </Card>
+            </Col>
+          )
+      )}
     </Row>
   );
 }
