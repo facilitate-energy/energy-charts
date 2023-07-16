b309429df76ff70965fabdb135e42c7b39358871
olejandro <ob@facilitate.energy>
2023-07-15 20:06:23 -0500

    Simplify some code

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -46,7 +46,7 @@ function Menu(props) {
           </Form.Select>
         </FloatingLabel>
 
-        <Collapse in={selectedScenarios[1] ? true : false}>
+        <Collapse in={!!selectedScenarios[1]}>
           <div>
             <hr className="menu-separator" />
             <Form.Switch
