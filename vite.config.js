8406b90e748999f5e9ee557c7b243430ce0e39c7
Olexandr Balyk <ob@facilitate.energy>
2025-10-21 21:04:46 +0300

    Migrate to rolldown-vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import path from "path";
 import { defineConfig } from "vite";
-import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
-import eslint from "vite-plugin-eslint";
 
 export default defineConfig(() => {
   return {
@@ -21,10 +19,8 @@ export default defineConfig(() => {
     },
     plugins: [
       react(),
-      eslint(),
       // svgr options: https://react-svgr.com/docs/options/
-      svgr({ svgrOptions: { icon: true } }),
-      legacy()
+      svgr({ svgrOptions: { icon: true } })
     ],
     test: {
       globals: true,
